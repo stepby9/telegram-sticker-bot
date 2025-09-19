128c003b0cdab4cbb961ca970638b2acd2de2219
stepby9 <78685883+stepby9@users.noreply.github.com>
2025-09-19 17:14:41 +0200

    Update bot.js

--- bot.js
+++ bot.js
@@ -12,7 +12,7 @@ const bot = new TelegramBot(BOT_TOKEN, { polling: true });
 // Your sticker mapping
 const stickerMap = {
     // Trigger sticker -> Response sticker
-    'CAACAgQAAxkBAAE7JUBoyBQM-l6Ii4L78U1ODXOcuX6t_QACEQsAAvRKEVPQ5z6DiIPfLDYE': 'CAACAgQAAxkBAAE7JzloyDpdU9u1ioEkQDSfdRyDVAMokgACwAkAAtxO0FKD4ZV3-kyfuDYE'
+    'CAACAgQAAyEFAASz83xjAAMTaMg8ue6oj_jhkyqVY-YyrkNkGYQAAhELAAL0ShFT0Oc-g4iD3yw2BA': 'CAACAgQAAyEFAASz83xjAAMYaMg9FGUur-PnhKWn2H-IVjSIUb4AAsAJAALcTtBSg-GVd_pMn7g2BA'
 };
 
 console.log('Bot is starting...');
